49f63690562977a1879b32357c7dc182640b8bd2
Interdott <56249049+Interdott@users.noreply.github.com>
2020-10-29 01:02:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 
 int main(void){
-     int NumV=0;// количество вершин
-     int NumN=0;// количество узлов
+     int ver=0;// количество вершин
+     int uzl=0;// количество узлов
      int is_directed=0;// ориентированность графа
      int is_weight=0;// взвешенность графа
      int c;
 	 FILE *MatrFile=fopen("matr.txt", "r");
 	 if(MatrFile==NULL){
-        printf("No such File");
+        printf("takogo faila net");
      }else{
-        printf("File is read\n");
+        printf("ctenie faila\n");
         while(!feof(MatrFile)){              
             if((fgetc(MatrFile))==';'){
-                NumV++;
+                ver++;
             }
         } 
         fclose(MatrFile);
         FILE *MatrFile=fopen("matr.txt", "r");
         while((c=fgetc(MatrFile))!=';'){
             if(c==',')
-                NumN++;
+                uzl++;
         }
-        NumN++;
+        uzl++;
         fclose(MatrFile);
-        int Arr [NumV][NumN]; // матрица инцидентности
+        int Arr [ver][uzl]; // матрица инцидентности
         MatrFile=fopen("matr.txt", "r");
-        for(int i=0;i<NumV;i++){
+        for(int i=0;i<ver;i++){
             int j=0;
             while((c=fgetc(MatrFile))!=';')
                 fscanf(MatrFile,"%d",&Arr[i][j++]);
@@ -37,8 +37,8 @@ int main(void){
 
 // проверяем является ли граф ориентрованным или взвешенным
         
-        for (int i = 0; i < NumV; i++){
-		    for (int j = 0; j < NumN; j++){
+        for (int i = 0; i < ver; i++){
+		    for (int j = 0; j < uzl; j++){
 			    printf("%d ",Arr[i][j]);
                 if (Arr[i][j]>1)
                     is_weight=1;
@@ -47,10 +47,10 @@ int main(void){
             }
             printf("\n");
 	    }  
-        char weight[NumN];// массив взвешенных узлов
+        char weight[uzl];// массив взвешенных узлов
         if(is_weight){
-            for (int j = 0; j < NumN; j++){
-                for (int i = 0; i < NumN; i++){
+            for (int j = 0; j < uzl; j++){
+                for (int i = 0; i < uzl; i++){
                     if (Arr[i][j]>0)
                         weight[j]=Arr[i][j];
                 }
@@ -58,13 +58,13 @@ int main(void){
             }
         }
     
-        char VertNames[NumV]; // массив вершин
-        for (int i = 0; i < NumV; i++)
+        char VertNames[ver]; // массив вершин
+        for (int i = 0; i < ver; i++)
             VertNames[i]='a'+i;
-        char Connect[NumN][2];// массив соединенных вершин
-        for (int j = 0; j < NumN; j++){
+        char Connect[uzl][2];// массив соединенных вершин
+        for (int j = 0; j < uzl; j++){
             int k=0;
-            for (int i = 0; i < NumV; i++){
+            for (int i = 0; i < ver; i++){
                 if(Arr[i][j]<0)
                     Connect[j][1]=VertNames[i];
                 if (Arr[i][j]>0)
@@ -72,7 +72,7 @@ int main(void){
              }
         }
         printf("\n");
-        for (int i = 0; i < NumN; i++){
+        for (int i = 0; i < uzl; i++){
 	        for (int j = 0; j < 2; j++){
                 if(!Connect[i][j]){
                     Connect[i][j]=Connect[i][j-1];
@@ -86,13 +86,13 @@ int main(void){
 
         FILE *GraphFile=fopen("graph.dot", "w");
         if(GraphFile==NULL){
-            printf("Problem with making a file");
+            printf("ne poluchaetsya sozdat fail");
         }else{
-            printf("File is read\n");
+            printf("chtenie\n");
             if(is_directed)
                 fprintf(GraphFile,"di");
             fprintf(GraphFile,"graph Test {\n");
-            for (int i = 0; i < NumN; i++){
+            for (int i = 0; i < uzl; i++){
                 if(!is_directed)
                     fprintf(GraphFile,"%c -- %c ",Connect[i][0],Connect[i][1]);
                 else{
@@ -109,10 +109,10 @@ int main(void){
         
         //проверка связности графа по теореме
 
-        if(NumN>(((NumV-1)*(NumV-2))/2)){
-            printf("This graph is connected\n");
+        if(uzl>(((ver-1)*(ver-2))/2)){
+            printf("svyazniy graph\n");
         }else{
-            printf("This graph is not connected\n");
+            printf("nesvyazniy graph\n");
         }
     }
  return 0;
